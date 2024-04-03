f1f0d47bcdc581930713a9dc613113ea97ef24d8
Breno Santana <contatobrenosantana@outlook.com>
2024-04-03 18:15:08 -0300

    Correção Erro Package

--- src/TodoList.java
+++ src/TodoList.java
@@ -1,51 +1,50 @@
-package src;
-
 import java.util.Scanner;
 
 public class TodoList {
     public static void main(String[] args) {
         int numero = 0;
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("Bem-vindo à Lista de Tarefas!\n"
-                    + "1. Adicionar Tarefa\n"
-                    + "2. Remover Tarefa\n"
-                    + "3. Marcar Tarefa como Concluída\n"
-                    + "4. Listar Tarefas\n"
-                    + "5. Limpar Tarefas Concluídas\n"
-                    + "6. Sair\n"
-                    + "Escolha uma opção:");
+        Scanner scanner = new Scanner(System.in);
 
-            int opcao = scanner.nextInt();
+        System.out.println("Bem-vindo à Lista de Tarefas!\n"
+                + "1. Adicionar Tarefa\n"
+                + "2. Remover Tarefa\n"
+                + "3. Marcar Tarefa como Concluída\n"
+                + "4. Listar Tarefas\n"
+                + "5. Limpar Tarefas Concluídas\n"
+                + "6. Sair\n"
+                + "Escolha uma opção:");
 
-            switch (opcao) {
-                case 1:
-                    System.out.println("Digite a tarefa a ser adicionada:");
-                    String tarefa = scanner.nextLine();
-                    adicionarTarefa(tarefa);
-                    break;
-                case 2:
-                    System.out.println("Digite o número da tarefa a ser removida:");
-                    numero = scanner.nextInt();
-                    removerTarefa(numero);
-                    break;
-                case 3:
-                    System.out.println("Digite o número da tarefa a ser marcada como concluída:");
-                    numero = scanner.nextInt();
-                    marcarTarefaComoConcluida(numero);
-                    break;
-                case 4:
-                    listarTarefas();
-                    break;
-                case 5:
-                    limparTarefasConcluidas();
-                    break;
-                case 6:
-                    System.out.println("Até mais!");
-                    break;
-                default:
-                    System.out.println("Opção inválida!");
-            }
+        int opcao = scanner.nextInt();
+
+        switch (opcao) {
+            case 1:
+                System.out.println("Digite a tarefa a ser adicionada:");
+                String tarefa = scanner.nextLine();
+                adicionarTarefa(tarefa);
+                break;
+            case 2:
+                System.out.println("Digite o número da tarefa a ser removida:");
+                numero = scanner.nextInt();
+                removerTarefa(numero);
+                break;
+            case 3:
+                System.out.println("Digite o número da tarefa a ser marcada como concluída:");
+                numero = scanner.nextInt();
+                marcarTarefaComoConcluida(numero);
+                break;
+            case 4:
+                listarTarefas();
+                break;
+            case 5:
+                limparTarefasConcluidas();
+                break;
+            case 6:
+                System.out.println("Até mais!");
+                break;
+            default:
+                System.out.println("Opção inválida!");
         }
+        scanner.close();
     }
 
     private static void adicionarTarefa(String tarefa) {
